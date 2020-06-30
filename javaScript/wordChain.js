48b5102b488e2f01e4f04b832eaa9343b2bd9ac6
jiyoungbkim <jiyoungbkim@gmail.com>
2020-06-30 22:03:32 +0900

    event

--- javaScript/wordChain.js
+++ javaScript/wordChain.js
@@ -2,21 +2,27 @@ var body = document.body;
 var word = document.createElement('div');
 word.textContent = '딸기';
 document.body.append(word);
+var form = document.createElement('form');
+document.body.append(form);
 var input = document.createElement('input');
-document.body.append(input);
+form.append(input);
 var button = document.createElement('button');
 button.textContent = '입력!';
-document.body.append(button);
+form.append(button);
 var result = document.createElement('div');
 document.body.append(result);
 
-button.addEventListener('click', function callback () {
+form.addEventListener('submit', function callback (event) {
+    event.preventDefault();
     if(word.textContent[word.textContent.length-1] === input.value[0]) {
         result.textContent = '딩동댕';
         word.textContent = input.value;
         input.value = '';
+        input.focus();
     } else {
         result.textContent = '땡';
+        input.value = '';
+        input.focus();
     }
 });
 /* var word = '딸기'
