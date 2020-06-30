6082bc7ea495e3c555512c1fd06e45646fe45b73
jiyoungbkim <jiyoungbkim@gmail.com>
2020-06-30 20:39:19 +0900

    js_html

--- javaScript/wordChain.js
+++ javaScript/wordChain.js
@@ -1,4 +1,16 @@
-var word = '딸기'
+var body = document.body;
+var word = document.createElement('div');
+word.textContent = '딸기';
+document.body.append(word);
+var input = document.createElement('input');
+document.body.append(input);
+var button = document.createElement('button');
+button.textContent = '입력!';
+document.body.append(button);
+var result = document.createElement('div');
+document.body.append(result);
+
+/* var word = '딸기'
 while(true) {
     var newWord = prompt(word);
     if(word[word.length-1] === newWord[0]) {
@@ -6,4 +18,4 @@ while(true) {
     } else {
         alert("끝말 잇기!");
     }
-}
+} */
