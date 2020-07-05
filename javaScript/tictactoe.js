c41fe53e291a53500e062ff92d45961a02d0914a
jiyoungbkim <jiyoungbkim@gmail.com>
2020-07-05 19:19:41 +0900

    tictactoe3
    X,O 입력

--- javaScript/tictactoe.js
+++ javaScript/tictactoe.js
@@ -2,8 +2,28 @@ var body = document.body;
 var table = document.createElement('table');
 var trArray = [];
 var tdArray = []; //빈배열
+var turn = 'X';
 var callback = function(event) {
-    console.log(event.target);
+    console.log(event.target); // 칸
+    console.log(event.target.parentNode); // 줄
+    console.log(event.target.parentNode.parentNode); // 테이블
+
+    var trNum = trArray.indexOf(event.target.parentNode);
+    console.log('몇줄', trNum);
+    var tdNum = tdArray[trNum].indexOf(event.target);
+    console.log('몇칸', tdNum);
+
+    if(tdArray[trNum][tdNum].textContent !== '' ) { //칸이 이미 채워져 있는가? 
+        console.log('Not Empty');            
+    } else {
+        console.log('Empty');
+        tdArray[trNum][tdNum].textContent = turn;
+        if(turn === 'X') {
+            turn = 'O';
+        } else {
+            turn = 'X';
+        } 
+    }
 };
 for(var i = 0; i < 3; i++) {
     var tr = document.createElement('tr');
@@ -12,10 +32,10 @@ for(var i = 0; i < 3; i++) {
     for(var j = 0; j < 3; j++) {
         var td = document.createElement('td');
         td.addEventListener('click', callback);
-        tdArray[i].push('td'); //칸이 들어간 배열
+        tdArray[i].push(td); //칸이 들어간 배열
         tr.appendChild(td);
     }
     table.appendChild(tr)
 }
 body.appendChild(table);
-console.log(trArray, tdArray);
+console.log('줄들', trArray, '칸들', tdArray);
