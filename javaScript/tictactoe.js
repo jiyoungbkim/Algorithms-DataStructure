1bc9044bb96043f79b2401c53a85f679d9d8c41f
jiyoungbkim <jiyoungbkim@gmail.com>
2020-07-15 16:08:23 +0900

    tictactoe4
    일치 검사

--- javaScript/tictactoe.js
+++ javaScript/tictactoe.js
@@ -15,15 +15,47 @@ var callback = function(event) {
 
     if(tdArray[trNum][tdNum].textContent !== '' ) { //칸이 이미 채워져 있는가? 
         console.log('Not Empty');            
-    } else {
+    } else { // 빈칸이면
         console.log('Empty');
         tdArray[trNum][tdNum].textContent = turn;
-        if(turn === 'X') {
-            turn = 'O';
-        } else {
-            turn = 'X';
-        } 
-    }
+        // 세칸이 다 채워졌나?
+        var full = false;
+        // 가로줄 검사
+        if(tdArray[trNum][0].textContent === turn && 
+            tdArray[trNum][1].textContent === turn && 
+            tdArray[trNum][2].textContent === turn){
+            full = true;
+        }
+        // 세로줄 검사
+        if(tdArray[0][tdNum].textContent === turn && 
+            tdArray[1][tdNum].textContent === turn && 
+            tdArray[2][tdNum].textContent === turn){
+            full = true;
+        }
+        // 대각선 검사
+        if(trNum - tdNum === 0 || Math.abs(trNum - tdNum) === 2) { // 대각선 검사 필요한 경우
+            if(tdArray[0][0].textContent === turn &&
+                tdArray[1][1].textContent === turn &&
+                tdArray[2][2].textContent === turn ){
+                full = true;    
+            }
+            if(tdArray[0][2].textContent === turn &&
+                tdArray[1][1].textContent === turn &&
+                tdArray[2][0].textContent === turn ){
+                full = true;
+            }
+        }
+        // 다 찼으면 
+        if(full){ // 조건문에 ===true는 생략 가능
+            console.log(turn + '님이 승리!');
+        } else { // 다 안찼으면
+            if(turn === 'X') {
+                turn = 'O';
+            } else {
+                turn = 'X';
+            } 
+        }      
+    }   
 };
 for(var i = 0; i < 3; i++) {
     var tr = document.createElement('tr');
