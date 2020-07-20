364bae8d6f2645dfc56d5355caa6d90b69e9c467
jiyoungbkim <jiyoungbkim@gmail.com>
2020-07-20 16:58:28 +0900

    rockpassci2
    dictionary

--- javaScript/rockpasci.js
+++ javaScript/rockpasci.js
@@ -1,19 +1,23 @@
-var left = 0;
+var comsChoice = 0;
+var dictionary = { // 딕셔너리 자료구조
+    rock : '0',
+    paper : '-380px',
+    scissors : '-770px'
+};
 setInterval(function () {
-    if (left === 0) {
-        left = '-380px';
-    } else if (left === '-380px') {
-        left = '-770px';
+    if (comsChoice === dictionary.rock) {
+        comsChoice = dictionary.paper;
+    } else if (comsChoice === dictionary.paper) {
+        comsChoice = dictionary.scissors;
     } else {
-        left = 0;
+        comsChoice = dictionary.rock;
     }
     document.querySelector('#computer').style.background = 
-    'url(../image/rock-paper-scissors.png) ' + left + ' 0';
+    'url(../image/rock-paper-scissors.png) ' + comsChoice + ' 0';
 }, 100);
 
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function() {
-        console.log(this.textContent);
+        console.log(this.textContent, comsChoice);
     });        
 });
-
