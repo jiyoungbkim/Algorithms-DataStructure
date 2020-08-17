bea26994f91cbcb9614e66e4552b84457ac165c0
jiyoungbkim <jiyoungbkim@gmail.com>
2020-08-17 22:39:40 +0900

    변수, 배열, 반복문 4

---
+++ JavaDataStructure/Chapter1/Code08.java
@@ -0,0 +1,25 @@
+package JavaDataStructure.Chapter1;
+
+import java.util.Scanner;
+
+public class Code08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] data = new int[n];
+        for(int i = 0; i < n; i++) {
+            data[i] = sc.nextInt();
+        }
+        sc.close();
+        int sum = 0;
+        int max = data[0];
+        for(int i = 0; i < n; i++){
+            sum += data[i];
+            if(max < data[i]) {
+                max = data[i];
+            }
+        }
+        System.out.println("The sum is" + sum);
+        System.out.println("The maximum is" + max);
+    }
+}
---
+++ JavaDataStructure/Chapter1/Code09.java
@@ -0,0 +1,26 @@
+package JavaDataStructure.Chapter1;
+
+import java.util.Scanner;
+
+public class Code09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] data = new int[n];
+        for(int i = 0; i < n; i++){
+            data[i] = sc.nextInt();
+        }
+        sc.close();
+
+        int tmp = data[n-1];
+        for(int i = n-2; i >= 0; i--){
+            data[i+1] = data[i];
+        }
+        data[0] = tmp;
+
+        for(int i = 0; i < n; i++){
+            System.out.print(data[i]+" ");
+        }
+        System.out.println();        
+    }
+}
