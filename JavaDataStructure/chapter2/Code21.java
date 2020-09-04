0c816e5536c83656a510c95d50f6eafbb1d97196
jiyoungbkim <jiyoungbkim@gmail.com>
2020-09-04 15:49:52 +0900

    gridrefactoring

--- JavaDataStructure/chapter2/Code21.java
+++ JavaDataStructure/chapter2/Code21.java
@@ -10,8 +10,7 @@ public class Code21 {
 
     public static void main(String[] args) {
         try {
-            Scanner inFile = new Scanner(new File("JavaDataStructure/chapter2/grid.txt"));
-           
+            Scanner inFile = new Scanner(new File("JavaDataStructure/chapter2/grid.txt"));           
             n = inFile.nextInt();
             grid = new int [n][n];
             for(int i = 0; i < n; i++){
@@ -20,11 +19,15 @@ public class Code21 {
                 }
             }            
             inFile.close();
+                
             for(int x = 0; x < n; x++){
                 for(int y = 0; y < n; y++){
-                    for(int dir = 0; dir < 8; dir++){
-                        for(int len = 1; len <= grid.length; len++){
-                            int val = computeValue(x,y,dir,len);
+                    int val = grid[x][y];
+                    if(isPrime(val))
+                        System.out.println(val);
+                    for(int dir = 0; dir < 8; dir++){                        
+                        for(int len = 2; len <= n; len++){
+                            val = computeValue(x,y,dir,len);
                             if(val != -1 && isPrime(val))
                                 System.out.println(val);
                         }
@@ -39,29 +42,38 @@ public class Code21 {
      
     static int computeValue(int x, int y, int dir, int len) {
         int value = 0;
-        for(int i = 0; i <= len; i++){
-            int digit = getDigit(x,y,dir,len);
-            if(digit == -1)
-                return -1;
+        for(int i = 0; i < len; i++){
+            int digit = getDigit(x,y,dir,i);
+            if(digit == -1){
+                 return -1;
+            }               
             value = value*10 + digit;
         }        
         return value;
     }
-
-    private static int getDigit(int x, int y, int dir, int len) {
-        return 0;
-    }
-
-    static boolean isPrime(int val) {
-        boolean isPrime = true;
-        for(int n = 2; n <= val; n++){   
-            isPrime = true;
-            for(int i = 2; i*i <= val; i++){
-                if(n % i == 0)
-                    isPrime = false;
-            }
-        }
-        return isPrime;
-    }
+    static int [] offsetX = {-1,-1,0,1,1,1,0,-1};
+    static int [] offsetY = {0,1,1,1,0,-1,-1,-1};
     
+    static int getDigit(int x, int y, int dir, int len) {
+        
+        int newX = x + len*offsetX[dir];
+        int newY = y + len*offsetY[dir];
+        if(newX < 0 || newX >= grid.length || newY < 0 || newY >= grid.length){
+            return -1;
+        }            
+        else {       
+            //System.out.println(grid[newX][newY]);     
+            return grid[newX][newY];            
+        }            
+    }
+
+    static boolean isPrime(int val) {   
+        if(val == 0 || val == 1) return false;  
+        for(int i = 2; i*i <= val; i++){
+            if(val % i == 0){
+                return false;
+            }                    
+        }        
+        return true;
+    }    
 }
