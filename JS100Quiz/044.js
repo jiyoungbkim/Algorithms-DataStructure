7ecb4a452d60753c294929b4eb99ff72af14e9f7
jiyoungbkim <jiyoungbkim@gmail.com>
2020-10-11 23:20:48 +0900

    type

--- JS100Quiz/044.js
+++ JS100Quiz/044.js
@@ -2,6 +2,6 @@ let num = prompt('숫자를 입력');
 let result = 0;
 while(num !== 0) {
     result += num % 10;
-    num = Math.floor(n/10);
+    num = Math.floor(num/10);
 }
 console.log(result);
---
+++ Python/Basic_6hours/01_type.py
@@ -0,0 +1,15 @@
+print(52,True,"Hello")
+print(str(52)+str(True)+"Hello")
+print("ha"*9) #9번 출력
+
+# 애완동물 소개
+animal = "cat"
+name = "cream"
+age = 4
+hobby = "taking a nap"
+is_adult = age >= 3
+
+print("My " + animal + "'s name is " + name)
+hobby = "playing with a ball"
+print(name + " is " + str(age) + "years old, she likes " + hobby + " very much.")
+print("Is " + name + " adult? " + str(is_adult))
