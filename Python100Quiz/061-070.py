d480a4785d4c75059308604bc4f30f24d687b4a7
jiyoungbkim <jiyoungbkim@gmail.com>
2020-10-24 23:16:33 +0900

    quiz066-069

--- Python100Quiz/061-070.py
+++ Python100Quiz/061-070.py
@@ -1,25 +1,25 @@
-# # 061
-# input_data = input("al.num : ")
-# data_set = list(set(input_data))
-# data_set.sort()
-# data_count = [input_data.count(i) for i in data_set]
-# for i in range(len(data_set)):
-#     print(data_set[i], data_count[i], sep="", end="")
-# print()
+# 061
+input_data = input("al.num : ")
+data_set = list(set(input_data))
+data_set.sort()
+data_count = [input_data.count(i) for i in data_set]
+for i in range(len(data_set)):
+    print(data_set[i], data_count[i], sep="", end="")
+print()
 
-# user_input = input()
-# s = ''
-# storeString = user_input[0]
-# count = 0
-# for i in user_input:
-#     if i == storeString:
-#         count += 1
-#     else:
-#         s += storeString + str(count)
-#         storeString = i
-#         count = 1
-# s += storeString + str(count)
-# print(s)
+user_input = input()
+s = ''
+storeString = user_input[0]
+count = 0
+for i in user_input:
+    if i == storeString:
+        count += 1
+    else:
+        s += storeString + str(count)
+        storeString = i
+        count = 1
+s += storeString + str(count)
+print(s)
 
 # 063
 data = input("abbreviation : ").split(' ')
@@ -56,3 +56,79 @@ for i, j in zip(a, b):
         c.append([j, i])
     count += 1
 print(c)
+
+# 066
+blocks = ['ABCDEF', 'BCAD', 'ADEFQRX', 'BEDFG', 'AEBFDGCH']
+rule = 'ABCD'
+def solution(blocks, rule):
+    answer = []
+    for b in blocks:
+        answer.append(order_check(b, rule))
+    return answer
+
+def order_check(b, rule):
+    tmp = rule.index(rule[0])
+    for text in b:
+        if text in rule:
+            if tmp > rule.index(text):
+                return '불가능'
+            tmp = rule.index(text)
+    return '가능'
+print(solution(blocks, rule))
+
+# 067
+n = int(input('Handshake :'))
+x = 0
+for i in range(1, n//2):
+    x += i
+    if x > n:
+        break
+print(i-(x-n), i+1)
+
+def solution(n):
+    people = 0
+    total = 0
+    while(True):
+        total = people*(people-1)/2
+        if n < total:
+            break
+        people+=1
+    times = int(people-(total-n)-1)
+    return [times, people]
+
+# 068
+schedule = ["12:30","13:20","14:13"]
+present = '12:40'
+p = list(map(int, present.split(':')))
+result = []
+for i in schedule:
+    h = list(map(int, i.split(':')))
+    remain = (h[0]*60 + h[1]) - (p[0]*60 + p[1])
+    if remain < 0 :
+        result.append('지나갔습니다')
+    else:
+        a = remain // 60
+        b = remain % 60
+        result.append(str(a).zfill(2)+'시간 '+str(b).zfill(2)+'분')
+print(result)
+
+# 069
+def isPrime(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+n = int(input('Goldbach\'s conjecture : '))
+a = n//2
+b = n//2
+if a%2 == 0:
+    a -= 1
+    b += 1
+while True:
+    if isPrime(a) and isPrime(b):
+        break
+    else :
+        a -= 2
+        b += 2
+print(a, b)
