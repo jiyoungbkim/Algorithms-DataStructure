6e0bfee304475adb39522311fed834f1941b3350
jiyoungbkim <jiyoungbkim@gmail.com>
2020-11-12 22:11:15 +0900

    strReverse

--- theDayBeforeCoding/Python/02_recursion_bin.py
+++ theDayBeforeCoding/Python/02_recursion_bin.py
@@ -1,3 +1,4 @@
+# 2진수 구하기
 # 반복문
 n = int(input('binary : '))
 result = ''
@@ -7,7 +8,16 @@ while True:
     else:
         result += '1'
     n = n//2
-    if n == 1 or n == 0:
+    if n == 1:
         result += str(n)
         print(result[::-1])
         break
+
+# 재귀함수   
+def binaryNum(n):
+    if n < 2:
+        return str(n)
+    else:
+        return str(binaryNum(n//2)) + str(n % 2)
+
+print(binaryNum(11))
---
+++ theDayBeforeCoding/Python/03_recursion_strRev.py
@@ -0,0 +1,29 @@
+# 문자열 뒤집기
+# 반복문
+result = ''
+for i in 'leehojun':
+    result = i + result
+print(result)
+
+# 재귀함수
+def strReverse(str):
+    if str == '':
+        return ''
+    else:
+        return strReverse(str[1:]) + str[0]
+print(strReverse('leehojun'))
+
+# 각 자리수의 합
+# 반복문
+x = 0
+for i in '2231':
+    x += int(i)
+print(x)
+
+# 재귀함수
+def strSum(num):
+    if num == '':
+        return 0
+    else:
+        return int(num[0]) + int(strSum(num[1:])) 
+print(strSum('2231'))
