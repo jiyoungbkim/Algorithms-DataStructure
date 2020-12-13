b1015ae3b61238a10f83d189fa3f6a106227dd26
jiyoungbkim <jiyoungbkim@gmail.com>
2020-12-13 15:33:31 +0900

    i*i <= n

--- JavaDataStructure/chapter1/section1/Code10_0.java
+++ JavaDataStructure/chapter1/section1/Code10_0.java
@@ -2,7 +2,7 @@ package JavaDataStructure.chapter1.section1;
 
 public class Code10_0 {
     public static void main(String[] args) {
-        int n = 2;
+        int n = 4;
 
         if(isPrime(n) == true) {
             System.out.println(n+"은 소수입니다.");
@@ -11,7 +11,7 @@ public class Code10_0 {
         }              
     }
     public static boolean isPrime(int n){
-        for(int i = 2; i*i < n; i++){
+        for(int i = 2; i*i <= n; i++){
             if(n % i == 0) return false;
         }
         return true;
--- JavaDataStructure/chapter1/section1/Code10_2.java
+++ JavaDataStructure/chapter1/section1/Code10_2.java
@@ -2,17 +2,18 @@ package JavaDataStructure.chapter1.section1;
 
 public class Code10_2 {
     public static void main(String[] args) {
-        for(int n = 2; n <= 100000; n++){
+        for(int n = 2; n <= 50; n++){
             boolean isPrime = true;
             // isPrime이 false면 반복문을 돌지 않도록 개선
-            for(int i = 2; i*i <= 100000 && isPrime; i++){ 
+            for(int i = 2; i*i <= n && isPrime; i++){ 
                 if(n % i == 0) {
                     isPrime = false;
                 }
             }
             if(isPrime){
-                System.out.println(n);
+                System.out.print(n+" ");
             }
         }
+        System.out.println();
     }
 }
