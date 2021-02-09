6e3f12d8da75e048af3fe02f0efebe6b7f170c65
jiyoungbkim <jiyoungbkim@gmail.com>
2021-02-09 21:51:50 +0900

    progression

--- Python/Python_forCT/Greedy/greedy02.py
+++ Python/Python_forCT/Greedy/greedy02.py
@@ -1,6 +1,5 @@
 # 큰 수의 법칙
 n, m, k = map(int, input().split())
-
 data = list(map(int, input().split()))
 
 data.sort()
@@ -18,5 +17,18 @@ while True:
         break
     answer += second
     m -= 1
+
+print(answer)
+
+# 반복되는 수열
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+# 가장 큰 수가 더해지는 횟수 계산
+cnt = int(m/(k+1)) * k + m % (k+1)
+answer = cnt * first + (m-cnt) * second
 
 print(answer)
