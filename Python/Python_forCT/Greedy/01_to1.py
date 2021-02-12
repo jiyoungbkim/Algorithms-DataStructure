b2b211d9d5ec60627f3a99c8f21a947fe0d1cc55
jiyoungbkim <jiyoungbkim@gmail.com>
2021-02-12 19:19:53 +0900

    to1

--- Python/Python_forCT/Greedy/01_to1.py
+++ Python/Python_forCT/Greedy/01_to1.py
@@ -16,7 +16,7 @@ n, k = map(int, input().split())
 result = 0
 while True:
     # N이 K로 나누어 떨어지는 수가 될 때까지 빼기
-    target = (n // k)
+    target = (n // k) * k
     result += (n - target)
     n = target
     if n < k:
