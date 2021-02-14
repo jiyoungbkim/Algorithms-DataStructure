5e9880cdb168bd8ff7d68578f24754350a025e7a
jiyoungbkim <jiyoungbkim@gmail.com>
2021-02-14 15:58:53 +0900

    hopscotch

--- programmers/Practice/Java/Level_2/p12913.java
+++ programmers/Practice/Java/Level_2/p12913.java
@@ -1,26 +1,17 @@
 package programmers.Practice.Java.Level_2;
-
+// 땅따먹기
+// hopscotch
 public class p12913 {
     public static void main(String[] args) {
         System.out.println(solution(new int[][]{{1,2,3,5},{5,6,7,8},{4,3,2,1}}));
     }
     public static int solution(int[][] land) {
-        int col = 5;
-        int col2 = 0;       
-        int answer = 0;
-        for(int i = 0; i < land.length; i++){    
-            int max = land[i][0];
-            int cnt = 0;     
-            while(cnt < land[i].length){
-                if(cnt != col && max < land[i][cnt]) {
-                    max = land[i][cnt];
-                    col2 = cnt;
-                }
-                cnt++;
-            }
-            col = col2;
-            answer += max;
+        for(int i = 1; i < land.length; i++){    
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
         }
-        return answer;
+        return Math.max(Math.max(land[land.length-1][0], land[land.length-1][1]), Math.max(land[land.length-1][2], land[land.length-1][3]));
     }
 }
