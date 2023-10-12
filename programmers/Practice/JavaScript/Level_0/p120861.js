d2b7da4920ae70e1a48873543b6e084301abcbe6
jiyoungbkim <jiyoungbkim@gmail.com>
2023-10-12 19:32:50 +0900

    캐릭터의 좌표

--- programmers/Practice/JavaScript/Level_0/p120861.js
+++ programmers/Practice/JavaScript/Level_0/p120861.js
@@ -8,18 +8,67 @@
 // 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
 function solution(keyinput, board) {
   const key = { up: [1, 1], down: [1, -1], left: [0, -1], right: [0, 1] };
-  const origin = [0, 0];
+  const zero = [0, 0];
   const [x, y] = board;
-  const 가로 = parseInt(x / 2);
-  const 세로 = parseInt(y / 2);
+  const width = Math.floor(x / 2);
+  const height = Math.floor(y / 2);
+
+  const isValid = (point, newNum) => {
+    if (point === 0) return newNum <= width && newNum >= -width;
+    if (point === 1) return newNum <= height && newNum >= -height;
+  };
+
   for (let x of keyinput) {
-    origin[key[x][0]] += key[x][1];
+    let temp = zero[key[x][0]] + key[x][1];
+    if (isValid(key[x][0], temp)) zero[key[x][0]] += key[x][1];
   }
-  return [
-    origin[0] > 가로 ? 가로 : origin[0],
-    origin[1] > 세로 ? 세로 : origin[1],
-  ];
+
+  return [zero[0], zero[1]];
   // return keyinput.map((x) => (origin[key[x][0]] += key[x][1]));
 }
 // console.log(solution(["left", "right", "up", "right", "right"], [11, 12]));
 console.log(solution(["down", "down", "down", "down", "down"], [7, 9]));
+
+// switch 이용
+function solution(keyinput, board) {
+  let res = [0, 0];
+  for (let p of keyinput) {
+    switch (p) {
+      case "left":
+        if (-res[0] < board[0] / 2 - 1) res[0]--;
+        break;
+      case "right":
+        if (res[0] < board[0] / 2 - 1) res[0]++;
+        break;
+      case "up":
+        if (res[1] < board[1] / 2 - 1) res[1]++;
+        break;
+      case "down":
+        if (-res[1] < board[1] / 2 - 1) res[1]--;
+        break;
+    }
+  }
+  return res;
+}
+
+// reduce 이용
+function solution(keyinput, board) {
+  let key = { right: [1, 0], up: [0, 1], down: [0, -1], left: [-1, 0] };
+
+  let rslt = keyinput
+    .map((v) => key[v])
+    .reduce(
+      (a, b) => {
+        if (
+          Math.abs(a[0] + b[0]) > board[0] / 2 ||
+          Math.abs(a[1] + b[1]) > board[1] / 2
+        )
+          return [a[0], a[1]];
+
+        return [a[0] + b[0], a[1] + b[1]];
+      },
+      [0, 0]
+    );
+
+  return rslt;
+}
